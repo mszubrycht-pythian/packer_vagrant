b60920dcaad71b6074c73cfee3ab918fa1c6c490
matts <matts@lampscaled.net>
2014-09-03 19:55:04 -0700

    bump up Ubuntu to 14.04.1
    fix Guest Additions issues
    
    update README
    
    clean up

--- scripts/root_setup.sh
+++ scripts/root_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -e
+# this script  is run as root
 
 echo ''
 echo "******************************"
@@ -8,18 +7,19 @@ echo "*** START OF root_setup.sh ***"
 echo "******************************"
 echo ''
 
+
 # Updating and Upgrading dependencies
-# ms 2014-09-01 clean up apt lists to avoid 'hash sum mismatch'
-sudo rm -fR /var/lib/apt/lists/*
-sudo apt-get update -y -qq > /dev/null
-sudo apt-get upgrade -y -qq > /dev/null
+# clean up apt lists first to avoid 'hash sum mismatch' sillyness
+rm -fR /var/lib/apt/lists/*
+apt-get update -y -qq > /dev/null
+apt-get upgrade -y -qq > /dev/null
 
 # Install necessary libraries for guest additions and Vagrant NFS Share
-sudo apt-get -y install linux-headers-$(uname -r) build-essential dkms nfs-common libssl-dev libreadline-dev zlib1g-dev cryptsetup 
+apt-get -y install linux-headers-$(uname -r) build-essential dkms nfs-common libssl-dev libreadline-dev zlib1g-dev cryptsetup ntp
 
 
-# Install necessary dependencies
-sudo apt-get -y install curl wget git vim tmux
+# Install good-to-haves
+apt-get -y install curl wget git vim tmux gawk zerofree
 
 
 # Setup sudo to allow no-password sudo for "admin"
@@ -29,25 +29,22 @@ cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 
-#Install Redis
-#sudo apt-get -y -q install libjemalloc1
-#wget -q http://d7jrzzvab3wte.cloudfront.net/checkbot/deb/redis-server_2.6.13-1_amd64.deb
-#sha1sum redis-server_2.6.13-1_amd64.deb | grep 'ab50cf037fd63e160946f8946b6d318cdf11800d'
-#dpkg -i redis-server_2.6.13-1_amd64.deb
-#rm redis-server_2.6.13-1_amd64.deb
 
+# Install project-specific packages
+apt-get -y install nodejs npm 
+apt-get clean 
 
-sudo apt-get -y install nodejs npm gawk ntp 
+# install guest additions
+mkdir /mnt/guestadditions
+mount -o loop VBoxGuestAdditions.iso /mnt/guestadditions
+/mnt/guestadditions/VBoxLinuxAdditions.run --nox11;  umount /mnt/guestadditions
+rm -f VBoxGuestAdditions.iso
+lsmod |grep vbox
+
 
 echo ''
 echo "****************************"
 echo "*** END OF root_setup.sh ***"
 echo "****************************"
 echo ''
-#sudo apt-get -y install node
 
-# Install Postgresql
-#sudo apt-get -y -q install postgresql libpq-dev postgresql-contrib
-
-# Set Password to test for user postgres
-#sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'test';"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,27 +8,15 @@ echo "*** START OF setup.sh ***"
 echo "*************************"
 echo ''
 
-
 echo "Instaling keys"
 
-# Installing vagrant keys
+# Vagrant key
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 cd ~/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 chown -R vagrant ~/.ssh
-
-
-
-##wget --retry-connrefused -q -O - https://raw.github.com/creationix/nvm/master/install.sh | sh
-##source ~/.nvm/nvm.sh
-
-##nvm install 0.10.18
-##nvm alias default 0.10.18
-
-##echo "source ~/.nvm/nvm.sh" >> ~/.bash_profile
-
 
 echo "***********************"
 echo "*** END OF setup.sh ***"
